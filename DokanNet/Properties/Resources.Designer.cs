b6e8b3d33dfc111c38a24761be0976961f146e23
gdh1995 <gdh1995@qq.com>
2022-06-23 20:38:46 +0800

    noop: rename the project

--- DokanNet/Properties/Resources.Designer.cs
+++ DokanNet/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Shaman.Dokan.Archive.DokanNet.Properties {
+namespace Dokan2.Archive.DokanNet.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Shaman.Dokan.Archive.DokanNet.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Shaman.Dokan.Archive.DokanNet.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Dokan2.Archive.DokanNet.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
